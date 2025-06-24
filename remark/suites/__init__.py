411aa1d475224a674c925b66e32c5412dfdc24d7
Patrick Cloke <clokep@patrick.cloke.us>
2025-06-24 16:33:12 -0400

    Lint

--- remark/suites/__init__.py
+++ remark/suites/__init__.py
@@ -1,6 +1,9 @@
+from types import ModuleType
+from typing import Optional
+
 from . import lrucache, lrucache_evict
 
-SUITES = [
+SUITES: list[tuple[ModuleType, Optional[int]]] = [
     (lrucache, None),
     (lrucache_evict, None),
 ]
